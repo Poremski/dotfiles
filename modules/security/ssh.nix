005e8b62a7c4f800ff0f59b814a368befeb9dd7d
Javier Poremski <javier@poremski.se>
2026-03-25 23:55:57 +0100

    Make SSH defaults explicit

--- modules/security/ssh.nix
+++ modules/security/ssh.nix
@@ -3,6 +3,17 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+
+    matchBlocks."*" = {
+      addKeysToAgent = "no";
+      compression = false;
+      forwardAgent = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
 
     matchBlocks."github.com" = {
       hostname = "github.com";
