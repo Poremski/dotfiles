c01cf3909ab684b2e50478241f8b04ae61dd41b4
Javier Poremski <javier@poremski.se>
2026-03-26 19:41:34 +0100

    Enable Home Manager file backups

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -24,6 +24,7 @@ in
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "hm-backup";
   home-manager.extraSpecialArgs = {
     inherit hostMeta;
   };
