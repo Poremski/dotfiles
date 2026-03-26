8a807eda3ddc1626bed6aa85c516fce8a14cfded
Javier Poremski <javier@poremski.se>
2026-03-26 21:16:43 +0100

    Make Firefox semi-declarative

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -4,5 +4,15 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
+
+    profiles.default = {
+      id = 0;
+      isDefault = true;
+
+      settings = {
+        # Allow declarative extensions to coexist with manually installed ones.
+        "extensions.autoDisableScopes" = 0;
+      };
+    };
   };
 }
