f47d90c3029349b8e0744ba8151aa2dc8a504994
Javier Poremski <javier@poremski.se>
2026-03-26 19:21:33 +0100

    Use Swedish regional time formatting in Plasma

--- home/javier/poremski.nix
+++ home/javier/poremski.nix
@@ -6,6 +6,12 @@
     DOTFILES_FQDN = "${hostMeta.name}.${hostMeta.domain}";
   };
 
+  xdg.configFile."plasma-localerc".text = ''
+    [Formats]
+    LANG=en_US.UTF-8
+    LC_TIME=sv_SE.UTF-8
+  '';
+
   programs.fish.shellAliases = {
     rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles#${hostMeta.name}";
   };
--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -7,10 +7,10 @@
 
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
-    "en_SE.UTF-8/UTF-8"
+    "sv_SE.UTF-8/UTF-8"
   ];
 
   i18n.extraLocaleSettings = {
-    LC_TIME = "en_SE.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
   };
 }
