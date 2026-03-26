a19c9e8ad2abc1d2664858783eef947a58b7b483
Javier Poremski <javier@poremski.se>
2026-03-26 18:15:35 +0100

    Use Swedish time formatting with English locale

--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -4,4 +4,13 @@
   time.timeZone = "Europe/Stockholm";
 
   i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "en_SE.UTF-8/UTF-8"
+  ];
+
+  i18n.extraLocaleSettings = {
+    LC_TIME = "en_SE.UTF-8";
+  };
 }
