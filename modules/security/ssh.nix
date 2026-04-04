db24c3b12fd76afa298613808cb253ade96af9fe
Javier Poremski <javier@poremski.se>
2026-04-05 01:08:40 +0200

    fix(ssh): install ssh config with secure permissions

--- modules/security/ssh.nix
+++ modules/security/ssh.nix
@@ -1,23 +1,26 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
+let
+  sshConfig = pkgs.writeText "ssh-config" ''
+    Host github.com
+      HostName github.com
+      User git
+
+    Host *
+      AddKeysToAgent no
+      Compression no
+      ForwardAgent no
+      ServerAliveInterval 0
+      ServerAliveCountMax 3
+      HashKnownHosts no
+      UserKnownHostsFile ~/.ssh/known_hosts
+  '';
+in
 {
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-
-    matchBlocks."*" = {
-      addKeysToAgent = "no";
-      compression = false;
-      forwardAgent = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-    };
-
-    matchBlocks."github.com" = {
-      hostname = "github.com";
-      user = "git";
-    };
-  };
+  home.activation.installSshConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD mkdir -p "$HOME/.ssh"
+    $DRY_RUN_CMD chmod 700 "$HOME/.ssh"
+    $DRY_RUN_CMD rm -f "$HOME/.ssh/config"
+    $DRY_RUN_CMD install -m 600 ${sshConfig} "$HOME/.ssh/config"
+  '';
 }
