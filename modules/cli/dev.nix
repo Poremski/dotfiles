1a57dd0df3b70d79537b6b25438efaff58b26ea1
Javier Poremski <javier@poremski.se>
2026-04-05 11:17:18 +0200

    feat(dev): configure npm user prefix

--- modules/cli/dev.nix
+++ modules/cli/dev.nix
@@ -5,6 +5,14 @@
     pkgs.nodejs
   ];
 
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.local/bin"
+  ];
+
+  home.file.".npmrc".text = ''
+    prefix=${config.home.homeDirectory}/.local
+  '';
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
