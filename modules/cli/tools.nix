86dec80b9d0f254de742cca3dc8989267e78a843
Javier Poremski <javier@poremski.se>
2026-04-05 12:40:10 +0200

    feat(cli): add tmux

--- modules/cli/tools.nix
+++ modules/cli/tools.nix
@@ -3,5 +3,30 @@
 {
   home.packages = [
     pkgs.htop
+    pkgs.tmux
   ];
+
+  home.file.".tmux.conf".text = ''
+    set -g mouse on
+    set -g history-limit 100000
+    set -g default-terminal "tmux-256color"
+    set -ga terminal-overrides ",xterm-256color:Tc"
+
+    set -g base-index 1
+    setw -g pane-base-index 1
+    set -g renumber-windows on
+
+    bind | split-window -h
+    bind - split-window -v
+    unbind '"'
+    unbind %
+
+    bind h select-pane -L
+    bind j select-pane -D
+    bind k select-pane -U
+    bind l select-pane -R
+
+    setw -g mode-keys vi
+    set -g status-style "bg=default"
+  '';
 }
