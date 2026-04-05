3e0b5da75b345def8c4c3df6a6820dbe672bb031
Javier Poremski <javier@poremski.se>
2026-04-05 15:42:48 +0200

    feat(dev): add cargo bin to path

--- modules/cli/dev.nix
+++ modules/cli/dev.nix
@@ -20,6 +20,7 @@
   ];
 
   home.sessionPath = [
+    "${config.home.homeDirectory}/.cargo/bin"
     "${config.home.homeDirectory}/.nix-config/bin"
     "${config.home.homeDirectory}/.local/bin"
   ];
